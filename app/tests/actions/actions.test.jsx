d24f0bd6f2f0ee04e2541b7bbdf6021eb8ee441a
Razvan <razvan.comsa@live.com>
2017-01-08 17:39:21 +0200

    heroku set

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import firebase, {firebaseRef} from 'app/firebase'
+import firebase, { firebaseRef } from 'app/firebase';
 
 const expect = require('expect');
 
